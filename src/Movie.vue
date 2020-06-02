5e55c8f8a0f62b0b0a1d479fd66310f5b3812b42
songtak <62980992+songtak@users.noreply.github.com>
2020-06-02 16:49:11 +0900

    Add files via upload

---
+++ src/Movie.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <h3>검색결과 : {{count}}</h3>
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                <tr>
+                    <th class="text-left">No.</th>
+                    <th class="text-left">순위</th>
+                    <th class="text-left">영화제목</th>
+                    <th class="text-left">집계일</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="item of navermovie" :key="item.movieSeq">
+                    <td>{{ item.movieSeq }}</td>
+                    <td>{{ item.rank }}</td>
+                    <td>{{ item.title }}</td>
+                    <td>{{ item.rankDate }}</td>
+                </tr>
+                </tbody>
+            </template>
+        </v-simple-table>
+        <div class="text-center" id="move">
+            <div style="margin:0 auto; width: 300px; height: 100px">
+            <span v-if="prev" style="width: 50px; height: 50px; border: 1px solid black">이전</span>
+                <span v-for='i of arr' :key="i" style="width: 50px; height: 50px; border: 1px solid black">{{i}}</span>
+            <span v-if="next" style="width: 50px; height: 50px; border: 1px solid black">다음</span>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { mapState } from "vuex";
+    export default {
+        el:'#next',
+        data() {
+            return {
+                prev: false,
+                next: true,
+                page: 1,
+                arr: [1,2,3,4,5]
+            };
+        },
+        computed: {
+            ...mapState({
+                navermovie: state => state.crawling.navermovie,
+                count: state => state.crawling.count
+            })
+        }
+    };
+</script>
+
---
+++ src/Retrieve.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h3>검색결과 : {{count}}</h3>
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                <tr>
+                    <th class="text-left">No.</th>
+                    <th class="text-left">이미지</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="item of bugsmusic" :key="item.seq">
+                    <td>{{ item.seq }}</td>
+                    <td><img :src="item.thumbnail"></td>
+                    <td>{{ item.artist }}</td>
+                    <td>{{ item.title }}</td>
+                </tr>
+                </tbody>
+            </template>
+        </v-simple-table>
+        <div class="text-center">
+            <v-pagination v-model="page" :length="5" :total-visible="5"></v-pagination>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { mapState } from "vuex";
+    export default {
+        data() {
+            return {
+                page: 1
+            };
+        },
+        computed: {
+            ...mapState({
+                bugsmusic: state => state.crawling.bugsmusic,
+                count: state => state.crawling.count
+            })
+        }
+    };
+</script>
+
